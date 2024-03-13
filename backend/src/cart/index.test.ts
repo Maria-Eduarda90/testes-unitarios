b81dd2ae198e49aed37d96a62782cb9f7a10593a
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-03-13 13:41:52 -0300

    chore: todos os teste de unidade feito e testados

--- backend/src/cart/index.test.ts
+++ backend/src/cart/index.test.ts
@@ -1,15 +1,61 @@
 import { Cart } from "./index";
 import { Product } from "../product";
 
+const makeCart = (): Cart => {
+    return new Cart();
+};
+
+const makeProduct = (name: string, price: number): Product => {
+    return new Product(name, price);
+};
+
+const makeSut = () => {
+    const stu = makeCart();
+
+    return {
+        stu,
+    };
+};
+
 describe("Cart", () => {
     it.only("should empty cart", () => {
-        const cart = new Cart();
-        expect(cart.items.length).toBe(0);
+        const { stu } = makeSut();
+        expect(stu.items.length).toBe(0);
     });
 
     it("should has one item in cart", () => {
-        const cart = new Cart();
-        const prod1 = new Product("mangá", 22);
-        cart.addItem(prod1);
+        const { stu } = makeSut();
+        expect(stu.items.length).toBe(0);
+        stu.addItem(makeProduct("mangá", 22));
+        expect(stu.items.length).toBe(1);
+    });
+    it("should have one item if add two and remove one", () => {
+        const { stu } = makeSut();
+        const product1 = makeProduct("mangá2", 15);
+        const product2 = makeProduct("mangá1", 22);
+        expect(stu.items.length).toBe(2);
+        stu.removeItem(product1);
+        stu.removeItem(product2);
+        expect(stu.isEmpty).toBeTruthy();
+    });
+
+    it("should total equals 10, with two products", () => {
+        const { stu } = makeSut();
+        stu.addItem(makeProduct("mangá1", 10));
+        stu.addItem(makeProduct("mangá2", 10));
+        expect(stu.total()).toBe(20);
+    });
+
+    it("should total equals 0, with empty cart", () => {
+        const { stu } = makeSut();
+        expect(stu.total()).toBe(0);
+    });
+
+    it("should clear cart", () => {
+        const { stu } = makeSut();
+        stu.addItem(makeProduct("mangá", 10));
+        stu.addItem(makeProduct("mangá2", 10));
+        stu.clear();
+        expect(stu.isEmpty).toBeTruthy();
     });
 });
